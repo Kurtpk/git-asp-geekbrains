07d7ddd9581969929725d4623707b7447ada5a18
Kurtpk <wins-90@mail.ru>
2018-10-24 19:17:41 +0300

    Решение домашнего задания asp уровень 2 урок 5

---
+++ WebStore_Igor_Tonshev/WebStore.Tests/HomeControllerTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore_Igor_Tonshev.Controllers;
+
+namespace WebStore.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        private HomeController _controller;
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            _controller = new HomeController();
+        }
+
+        [TestMethod]
+        public void Index_Returns_View()
+        {
+            // Arrange and act
+            var result = _controller.Index();
+
+            // Assert
+            Xunit.Assert.IsType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void ContactUs_Returns_View()
+        {
+            var result = _controller.ContactUs();
+            Xunit.Assert.IsType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void ErrorStatus_404_Redirects_to_NotFound()
+        {
+            var result = _controller.ErrorStatus("404");
+            var redirectToActionResult = Xunit.Assert.IsType<RedirectToActionResult>(result);
+            Xunit.Assert.Null(redirectToActionResult.ControllerName);
+            Xunit.Assert.Equal("NotFoundPage", redirectToActionResult.ActionName);
+        }
+
+        [TestMethod]
+        public void ErrorStatus_Antother_Returns_Content_Result()
+        {
+            var result = _controller.ErrorStatus("500");
+            var contentResult = Xunit.Assert.IsType<ContentResult>(result);
+            Xunit.Assert.Equal("Статуcный код ошибки: 500", contentResult.Content);
+        }
+
+        [TestMethod]
+        public void Checkout_Returns_View()
+        {
+            var result = _controller.Checkout();
+            Xunit.Assert.IsType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void BlogSingle_Returns_View()
+        {
+            var result = _controller.BlogSingle();
+            Xunit.Assert.IsType<ViewResult>(result);
+        }
+
+
+        [TestMethod]
+        public void Blog_Returns_View()
+        {
+            var result = _controller.Blog();
+            Xunit.Assert.IsType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void Error_Returns_View()
+        {
+            var result = _controller.Error();
+            Xunit.Assert.IsType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void NotFoundPage_Returns_View()
+        {
+            var result = _controller.NotFoundPage();
+            Xunit.Assert.IsType<ViewResult>(result);
+        }
+    }
+}
